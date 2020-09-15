7112d3dea74ada44bebb00389e123a3c9ad437b2
Leandro Nunes <nunes.pessoal@gmail.com>
2020-09-15 19:18:04 -0300

    The modal image and text changes dynamically

--- foodfy/scripts.js
+++ foodfy/scripts.js
@@ -2,13 +2,22 @@ const modalOverlay = document.querySelector('.modal-overlay')
 const cards = document.querySelectorAll('.card')
 
 for (let card of cards){
-    card.addEventListener("click", function(){        
-        modalOverlay.classList.add('active')        
+    card.addEventListener("click", function(){       
+        modalOverlay.classList.add('active')
+
+        const imageId = card.getAttribute('id')
+        modalOverlay.querySelector("img").src = `/assets/${imageId}.png`
+
+        const title = card.querySelector("h1")
+        modalOverlay.querySelector(".modal-title").textContent = title.textContent
+
+        const chef = card.querySelector("p")
+        modalOverlay.querySelector(".author").textContent = chef.textContent
+              
     })
 }
 
 document.querySelector(".close-modal").addEventListener("click", function(){
-    modalOverlay.classList.remove("active")    
+    modalOverlay.classList.remove("active")
+       
 })
-
-console.log("Hello world")
