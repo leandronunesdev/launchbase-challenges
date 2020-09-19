928f6d24e8b3f2d3fd8d94b70b4f687511aa9c06
Leandro Nunes <nunes.pessoal@gmail.com>
2020-09-19 18:09:30 -0300

    Creation of Data.js

--- foodfy-refatorado/server.js
+++ foodfy-refatorado/server.js
@@ -2,21 +2,43 @@ const express = require('express')
 const nunjucks = require('nunjucks')
 
 const server = express()
+const recipes = require("./data")
 
 server.use(express.static('public'))
 server.set("view engine", "njk")
 
 nunjucks.configure("views", {
-    express:server
+    express:server,
+    autoescape: false,
+    noCache: true
 })
 
 server.get("/", function(req, res){
-    return res.render("index")
+    return res.render("index", {items: recipes})
 })
 
 server.get("/recipes", function(req, res){
-    return res.render("recipes")
+    return res.render("recipes", {items: recipes})
 })
+
+server.get("/recipes/:index", function (req, res) {    
+    const recipeIndex = req.params.index;
+
+    return res.render("recipe")  
+    console.log(recipes[recipeIndex])
+  
+    /*const recipe = recipes.find(function(recipe){
+        if (recipe.recipeIndex == recipeIndex){
+            return true
+        }
+    })
+
+    if (!recipe) {
+        return res.send("Recipe not found!")
+    }
+
+    return res.render("recipe", {item: recipe})*/
+  })
 
 server.get("/about", function(req, res){
     return res.render("about")
